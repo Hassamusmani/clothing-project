971f3afc38672d386a96fc53f3b45e88219fef3c
husmani <husmani@xiqinc.com>
2023-01-01 08:46:19 +0500

    sticky header added

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
@@ -13,13 +13,31 @@ import { closeCartDropdown } from "../../redux/cart/cart-actions";
 
 const Header = ({ currentUser, showCart, toggleCartHidden }) => {
   const location = useLocation();
+  const [headerBackground, setHeaderBackground] = useState(false);
+
+  const listenScrollEvent = () => {
+    if (window.scrollY > 50) {
+      setHeaderBackground(true);
+    }
+    else {
+      setHeaderBackground(false);
+    }
+  }
 
   useEffect(() => {
     if (showCart) toggleCartHidden();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }, [location]);
 
+  useEffect(() => {
+    window.addEventListener('scroll', listenScrollEvent);
+
+    return () =>
+      window.removeEventListener('scroll', listenScrollEvent);
+  }, []);
+
   return (
-    <div className="header">
+    <div className={`header ${headerBackground ? 'header-scroll' : ''}`}>
       <Link className="logo-container" to={'/'}>
         <Logo />
       </Link>
